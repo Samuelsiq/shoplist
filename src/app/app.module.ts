7c416c2d2e27cdfb5506aa1439cc23cb5eebe42d
Samuel <samuelferrer19@gmail.com>
2022-10-24 19:44:19 -0300

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { HeaderComponent } from './header.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shoping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopingListComponent,
+    RecipeBookComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent,
+
   ],
   imports: [
     BrowserModule
